d364b733ff140baaf06e6481114c34cc3b2c982c
kreedyX <55585560+kreedyX@users.noreply.github.com>
2022-04-24 19:23:59 +0200

    added premake and GLFW

---
+++ make_projects.bat
@@ -0,0 +1,2 @@
+vendor\premake5\premake5.exe vs2019
+pause
---
+++ premake5.lua
@@ -0,0 +1,120 @@
+workspace "Proton2D"
+	architecture "x64"
+    startproject "sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "vendor/GLFW/include"
+include "/vendor/GLFW"
+
+
+project "proton2d"
+	location "proton2d"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "%{prj.location}/src/pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "latest"
+
+		defines
+		{
+			"PROTON_PLATFORM_WINDOWS",
+			"PROTON_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} \"%{cfg.buildtarget.relpath}\" \"../bin/" .. outputdir .. "/sandbox\"")
+		}
+
+	filter "configurations:Debug"
+		defines "PROTON_DEBUG"
+		symbols "On"
+		buildoptions "/MDd"
+        runtime "Debug"
+
+	filter "configurations:Release"
+		defines "PROTON_RELEASE"
+		optimize "On"
+		buildoptions "/MD"
+        runtime "Release"
+
+
+project "sandbox"
+	location "sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"proton2d/src"
+	}
+
+	links
+	{
+		"proton2d"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "latest"
+
+		defines
+		{
+			"PROTON_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PROTON_DEBUG"
+		symbols "On"
+		buildoptions "/MDd"
+        runtime "Debug"
+
+	filter "configurations:Release"
+		defines "PROTON_RELEASE"
+		optimize "On"
+		buildoptions "/MD"
+        runtime "Release"
