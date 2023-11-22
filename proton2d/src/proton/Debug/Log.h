2ce9359a2099d3c4f0e1cac291b19d29b8bacfa3
Damian Żuk <55585560+Damian-Zuk@users.noreply.github.com>
2023-11-22 23:55:28 +0100

    Fix: Missing Debug directory in proton/src/proton

---
+++ proton2d/src/proton/Debug/Log.h
@@ -0,0 +1,64 @@
+#pragma once
+/*
+* From Hazel Engine:
+* https://github.com/TheCherno/Hazel/blob/master/Hazel/src/Hazel/Core/Log.h
+*/
+
+#include "proton/Core/Base.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
+// This ignores all warnings raised inside external headers
+#pragma warning(push, 0)
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
+
+namespace proton {
+
+	class Log
+	{
+	public:
+		static void Init();
+
+		static Shared<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		static Shared<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static Shared<spdlog::logger> s_CoreLogger;
+		static Shared<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
+{
+	return os << glm::to_string(quaternion);
+}
+
+// Core log macros
+#define PT_CORE_TRACE(...)    ::proton::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define PT_CORE_INFO(...)     ::proton::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define PT_CORE_WARN(...)     ::proton::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define PT_CORE_ERROR(...)    ::proton::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define PT_CORE_CRITICAL(...) ::proton::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define PT_TRACE(...)         ::proton::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define PT_INFO(...)          ::proton::Log::GetClientLogger()->info(__VA_ARGS__)
+#define PT_WARN(...)          ::proton::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define PT_ERROR(...)         ::proton::Log::GetClientLogger()->error(__VA_ARGS__)
+#define PT_CRITICAL(...)      ::proton::Log::GetClientLogger()->critical(__VA_ARGS__)
